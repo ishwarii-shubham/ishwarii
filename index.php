68baa7bac2ada7c563ea360bcf6fc8ffe8af7f67
varunsingh1996 <varun.s@meawwworld.com>
2019-06-02 13:47:11 +0530

    full testimonial added

--- index.php
+++ index.php
@@ -93,21 +93,28 @@
 			<div class="cont">
 				<div class="content">
 					<div class="content-top-bottom">
-						<h2>Our Story</h2>
-						<div class="col-md-6 men">
-							<img class="img-responsive" src='images1/our-story.jpg'>
-						</div>
-						<div class="col-md-6">
-							<div class = 'our-story'>
-								<p>Majestic mountains. Calming valleys. Gurgling water falls. Brilliant blue skies. If you are in the midst of all this, you are probably in the slice of paradise called Kinnaur.</p>
-								<p>We call this place — where nature flourishes untouched and unadulterated — ISHWARII, meaning heavenly.</p>
-								<p>Kinnaur is so much more than the tourist destination the world knows it as. In the lap of the mountains here flourish the healthiest and purest food packed with nutrition and high on health and medicinal benefits.</p>
-								<p>This is where ISHWARII comes in. ISHWARII aims to bring directly to you in the city a slice of life in Kinnaur.</p>
-								<p>ISHWARII believes everyone in the city has the right to pure food. We source preservative-free, locally-farmed food products directly from the natives of Kinnaur without involving any middlemen.</p>
-								<p>ISHWARII knows you cannot control the air you breath. But you can choose the food you consume.</p>
+						<!-- <h2 class="section-title">Our Story</h2> -->
+						<div class="row">
+							<div style="text-align: center;">
+								<h2 class="section-title">Our Story</h2>
 							</div>
 						</div>
-						<div class="clearfix"> </div>
+						<div>
+							<div class="col-md-6 men">
+								<img class="img-responsive" src='images1/our-story.jpg'>
+							</div>
+							<div class="col-md-6">
+								<div class = 'our-story'>
+									<p>Majestic mountains. Calming valleys. Gurgling water falls. Brilliant blue skies. If you are in the midst of all this, you are probably in the slice of paradise called Kinnaur.</p>
+									<p>We call this place — where nature flourishes untouched and unadulterated — ISHWARII, meaning heavenly.</p>
+									<p>Kinnaur is so much more than the tourist destination the world knows it as. In the lap of the mountains here flourish the healthiest and purest food packed with nutrition and high on health and medicinal benefits.</p>
+									<p>This is where ISHWARII comes in. ISHWARII aims to bring directly to you in the city a slice of life in Kinnaur.</p>
+									<p>ISHWARII believes everyone in the city has the right to pure food. We source preservative-free, locally-farmed food products directly from the natives of Kinnaur without involving any middlemen.</p>
+									<p>ISHWARII knows you cannot control the air you breath. But you can choose the food you consume.</p>
+								</div>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
 					</div>
 					<div class="content-top">
 						<h1>NEW PRODUCTS</h1>
@@ -173,7 +180,7 @@
 					</div>
 
 					<!-- Feedback -->
-					<div class="content-top-bottom" style = "margin-top: 10px; margin-bottom: 10px; margin-right: -5%; margin-left: -5%;">
+					<div class="content-top-bottom" style = "margin-top: 2%; margin-bottom: 2%; margin-right: -5%; margin-left: -5%;">
 						<section>
 							<div class="customer-feedback">
 								<div class="container text-center">
@@ -253,6 +260,15 @@
 
 										</div><!-- /End col -->
 									</div><!-- /End row -->
+
+									<div class="row">
+										<div class="col-sm-offset-2 col-sm-8">
+											<div>
+												<a href="#0" class="cd-see-all">See All</a>
+											</div>
+										</div><!-- /End col -->
+									</div><!-- /End row -->
+
 								</div><!-- /End container -->
 							</div><!-- /End customer-feedback -->
 						</section>
@@ -262,6 +278,158 @@
 			</div>
 			<!---->
 		</div>
+
+		<div class="cd-testimonials-all">
+			<div class="cd-testimonials-all-wrapper">
+				<ul>
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit totam saepe iste maiores neque animi molestias nihil illum nisi temporibus.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-1.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nostrum nisi, doloremque error hic nam nemo doloribus porro impedit perferendis. Tempora, distinctio hic suscipit. At ullam eaque atque recusandae modi fugiat voluptatem laborum laboriosam rerum, consequatur reprehenderit omnis, enim pariatur nam, quidem, quas vel reiciendis aspernatur consequuntur. Commodi quasi enim, nisi alias fugit architecto, doloremque, eligendi quam autem exercitationem consectetur.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-2.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quibusdam eveniet, molestiae laborum voluptatibus minima hic quasi accusamus ut facere, eius expedita, voluptatem? Repellat incidunt veniam quaerat, qui laboriosam dicta. Quidem ducimus laudantium dolorum enim qui at ipsum, a error.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-3.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero voluptates officiis tempore quae officia! Beatae quia deleniti cum corporis eos perferendis libero reiciendis nemo iusto accusamus, debitis tempora voluptas praesentium repudiandae laboriosam excepturi laborum, nisi optio repellat explicabo, incidunt ex numquam. Ullam perferendis officiis harum doloribus quae corrupti minima quia, aliquam nostrum expedita pariatur maxime repellat, voluptas sunt unde, inventore.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-4.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit totam saepe iste maiores neque animi molestias nihil illum nisi temporibus.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-5.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quia quas, quis illo adipisci voluptate ex harum iste commodi nulla dolor. Eius ratione quod ab!</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-6.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dignissimos iure rem fugiat consequuntur officiis.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-1.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At temporibus tempora necessitatibus reiciendis provident deserunt maxime sit id. Dicta aut voluptatibus placeat quibusdam vel, dolore.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-2.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis iusto sapiente, excepturi velit, beatae possimus est tenetur cumque fugit tempore dolore fugiat! Recusandae, vel suscipit? Perspiciatis non similique sint suscipit officia illo, accusamus dolorum, voluptate vitae quia ea amet optio magni voluptatem nemo, natus nihil.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-3.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quasi officiis pariatur, fugit minus omnis animi ut assumenda quod commodi, ad a alias maxime unde suscipit magnam, voluptas laboriosam ipsam quibusdam quidem, dolorem deleniti id.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-4.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At temporibus tempora necessitatibus reiciendis provident deserunt maxime sit id. Dicta aut voluptatibus placeat quibusdam vel, dolore.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-5.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+
+					<li class="cd-testimonials-item">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque tempore ipsam, eos suscipit nostrum molestias reprehenderit, rerum amet cum similique a, ipsum soluta delectus explicabo nihil repellat incidunt! Minima magni possimus mollitia deserunt facere, tempore earum modi, ea ipsa dicta temporibus suscipit quidem ut quibusdam vero voluptatibus nostrum excepturi explicabo nulla harum, molestiae alias. Ab, quidem rem fugit delectus quod.</p>
+
+						<div class="cd-author">
+							<img src="img/avatar-6.jpg" alt="Author image">
+							<ul class="cd-author-info">
+								<li>MyName</li>
+								<li>CEO, CompanyName</li>
+							</ul>
+						</div> <!-- cd-author -->
+					</li>
+				</ul>
+			</div>	<!-- cd-testimonials-all-wrapper -->
+
+			<a href="#0" class="close-btn">Close</a>
+		</div> <!-- cd-testimonials-all -->
 		<!--//content-->
 		<!-- Add this before end body tag -->
 		<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
@@ -276,9 +444,12 @@
 				cycle: true
 				});
 		</script>
-
+		<!--Feedback Js  -->
 		<script src="js/feedback.js"></script>
 		<script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
+		<script src="js/masonry.pkgd.min.js"></script>
+		<script src="js/jquery.flexslider-min.js"></script>
+		<script src="js/testimonialfull.js"></script>
 
 		<?php include('assets/footer.php'); ?>
 
